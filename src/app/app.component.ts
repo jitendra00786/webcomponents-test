0244f72628757f92a91ef20dcae7ab251634fb3c
Jitendra Uppuluri <jitendrau@stg.com>
2019-09-25 12:19:50 +0530

    written fSlidemethod in TS file to get the desired image by event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'webcomponents-test';
+  
+
+  slideIndex:number = 4;
+
+
+  fSlide(n){
+   
+    this.showSlides(this.slideIndex = n);
+  }
+  
+    ngOnInit () {  }
+  
+
+
+      showSlides(slideIndex);
+  
+  
+   showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("flowerSlides");
+    var dots = document.getElementsByClassName("demo");
+    
+    if (n > slides.length) {this.slideIndex = 1}
+    if (n < 1) {this.slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[this.slideIndex-1].style.display = "block";
+    dots[this.slideIndex-1].className += " active";
+}
+
+ 
 }
